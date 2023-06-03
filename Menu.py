a7d9ee1ddb2085af511172e7b90ad064a5901706
AllanSoaresAnderson <allansoaresanderson@gmail.com>
2023-06-02 21:58:22 -0300

    Criação de um menu e do jogo da forca

---
+++ Menu.py
@@ -0,0 +1,18 @@
+import advinhacao
+import forca
+
+print("*********************************")
+print("********Jogos em Python!*********")
+print("*********************************")
+
+print("Qual jogo você quer jogar?")
+print("(1) Advinhação (2) Forca")
+escolha = int(input("Digite qual jogo você quer jogar: "))
+
+if escolha == 1:
+    print("Começando o jogo de adivinhação [...]")
+    advinhacao.jogar()
+else:
+    print("Começando o jogo de forca [...]")
+    forca.jogar()
+
