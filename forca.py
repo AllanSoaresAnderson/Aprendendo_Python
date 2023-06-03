cd48820488917be8a3694bb323755cc71f80c79c
AllanSoaresAnderson <allansoaresanderson@gmail.com>
2023-06-03 07:20:06 -0300

    Métodos para tratar Strings

--- forca.py
+++ forca.py
@@ -6,6 +6,22 @@ def jogar():
 
     print("Fim do jogo")
 
+    palavra_secreta = "banana"
+    acertou = False
+    enforcou = False
+
+    while (not acertou and not enforcou):
+        chute = input("Digite uma letra: ").strip()
+
+        index =0
+        for letra in palavra_secreta:
+
+            if (chute.lower() == letra.lower()):
+                print ("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
+        print ("jogando")
+
 
 if __name__ == "__main__":
     jogar()
