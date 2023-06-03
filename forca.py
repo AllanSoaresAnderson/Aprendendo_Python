a2e31559d8e958a24de1c2889d67ef99e3c6ff5f
AllanSoaresAnderson <allansoaresanderson@gmail.com>
2023-06-03 07:47:59 -0300

    Listas em python

--- forca.py
+++ forca.py
@@ -7,20 +7,19 @@ def jogar():
     print("Fim do jogo")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    print(letras_acertadas)
     acertou = False
     enforcou = False
 
     while (not acertou and not enforcou):
         chute = input("Digite uma letra: ").strip()
-
         index =0
         for letra in palavra_secreta:
-
             if (chute.lower() == letra.lower()):
-                print ("Encontrei a letra {} na posição {}".format(letra, index))
+                letras_acertadas[index] = chute.lower()
             index = index + 1
-
-        print ("jogando")
+        print (letras_acertadas)
 
 
 if __name__ == "__main__":
